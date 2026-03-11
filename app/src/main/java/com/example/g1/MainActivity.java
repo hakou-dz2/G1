e54187482d430a82dbb9d659e5d7c162d2a049b2
hakou <hakou.silem@gmail.com>
2026-03-11 13:46:22 +0100

    I linked java with XML

--- app/src/main/java/com/example/g1/MainActivity.java
+++ app/src/main/java/com/example/g1/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.g1;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +15,52 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText username,password;
+    TextView reset;
+    Button login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+
+        username = findViewById(R.id.et_username);
+        password = findViewById(R.id.et_pass);
+
+        reset = findViewById(R.id.tv_reset);
+
+        login = findViewById(R.id.btn_login);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String n = username.getText().toString();
+                String p = password.getText().toString();
+                if(p.length()<8){
+                    Toast.makeText(MainActivity.this, "password must be more than 8", Toast.LENGTH_SHORT).show();
+                }
+                if(n.isEmpty() && p.isEmpty()){
+                    Toast.makeText(MainActivity.this, "All fields must be filled", Toast.LENGTH_SHORT).show();
+                }else {
+                    if (!n.equals("SI")) {
+                        Toast.makeText(MainActivity.this, "the username is wrong", Toast.LENGTH_SHORT).show();
+
+                    } else {
+                        if (!p.equals("1234")) {
+                            Toast.makeText(MainActivity.this, "the password is wrong", Toast.LENGTH_SHORT).show();
+
+                        } else {
+                            Intent intent = new Intent(MainActivity.this, WelcomeActivity.class);
+                            intent.putExtra("username", n);
+                            startActivity(intent);
+                        }
+                    }
+
+                }}
         });
+
+
+
+
     }
 }
---
+++ app/src/main/java/com/example/g1/WelcomeActivity.java
@@ -0,0 +1,27 @@
+package com.example.g1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        TextView welcome = findViewById(R.id.tv_welcome);
+
+        Intent intent = getIntent();
+        if(intent != null) {
+            welcome.setText("Welcome " + intent.getStringExtra("username"));
+        }
+
+    }
+}
